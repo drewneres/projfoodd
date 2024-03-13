dd147242f5b9614d2aa689c1a87c50ee3b4dbb8f
Hendrew Neres <hendrewneresfsa@hotmail.com>
2024-03-12 21:49:55 -0300

    aula 2

---
+++ admin/includes/cabecalho.php
@@ -0,0 +1,10 @@
+<header>
+    <img src="img/site/ifood.png">
+
+    <div class="fixe-top container-fluid bg-dark mb-3 text-center" id="cabe" >
+        <form class="form-inline my-2 my-lg-0">
+        
+        </form>
+    </div>
+</header>
+
--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -1,16 +1,13 @@
 
-<nav class="navbar navbar-dark bg-dark flex-column col-2">
-    <div class= "row">
-        <div class="navbar-nav" id="navmen">
+<ul class="navbar navbar-dark bg-dark nav-pills nav-fill id='navmen' ">
+            <li class="nav-item"><a class="nav-link text-white" href="?secao=cadcomid">COMIDA CADASTRO</a></li>
+            <li class="nav-item"><a class="nav-link text-white"  href="?secao=manutecomida"">MANTER COMIDA</a></li>
+            <li class="nav-item"><a class="nav-link text-white" href="?secao=cadastrarestaurante"">CADASTRO DE RESTAURANTE</a></li>
+            <li class="nav-item"><a class="nav-link text-white" href="?secao=manuterest">MANTER RESTAURANTE</a></li>
+            <li class="nav-item"><a class="nav-link text-white" href="?secao=listarusuarios">LISTAR USUÁRIOS</a></li>
+            <li class="nav-item"><a class="nav-link text-white" href="index.php">LOGOUT</a></li>
+            <fo class="form-inline my-1 my-lg-0">
+            <input class="form-control mr-sm-1" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            </form>
 
-            <li class="nav-link"><a href="?secao=cadcomid">COMIDA CADASTRO</a></li>
-            <li class="nav-link"><a  href="?secao=manutecomida"">MANTER COMIDA</a></li>
-            <li class="nav-link"><a href="?secao=cadastrarestaurante"">CADASTRO DE RESTAURANTE</a></li>
-            <li class="nav-link"><a href="?secao=manuterest">MANTER RESTAURANTE</a></li>
-            <li class="nav-link"><a href="?secao=listarusuarios">LISTAR USUÁRIOS</a></li>
-            <li class="nav-link"><a href="index.php">LOGOUT</a></li>
-    </div>
-        </div>
-
-     
-</nav>
+</ul>
