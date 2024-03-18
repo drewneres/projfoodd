48a18b293d06ec53490949cbfc2f28aea67d3325
drewneres <hendrewneresfsa@hotmail.com>
2024-03-18 11:51:34 -0300

    restaurante

--- secoes/restaurantes.php
+++ secoes/restaurantes.php
@@ -1,1 +1,20 @@
-<h1>restaurante</h1>
+<?php
+include_once("dao/manipuladados.php");
+
+$dados = new ManipulaDados();   
+$dados->setTable('tb_restaurante'); 
+$lista = $dados->getAllDataTable(); 
+
+?>
+<section>
+    <?php 
+    foreach ($lista as $restaurante){
+        ?>
+        <h1> <?= $restaurante["nome"] ?> </h1>
+        <h2> <?= $restaurante["descricao"] ?> </h2>
+        <h3> <?= $restaurante["categoria"] ?> </h3>
+        <img src="<?= $restaurante["url"]?>" class="img-fluid rounded-start">
+        <?php
+    }
+    ?>
+</section>
