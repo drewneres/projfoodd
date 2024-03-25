04a1b91f3b5cb2e531008a5e0ca51b9a1eca5410
drewneres <hendrewneresfsa@hotmail.com>
2024-03-25 10:41:47 -0300

    Labels para editar/alterar

--- admin/controller/manterRestaurante.php
+++ admin/controller/manterRestaurante.php
@@ -8,6 +8,10 @@
 
 
     $id = $_POST['txtId'];
+    $nome= $_POST['txtNome'];
+    $descricao= $_POST['txtDescricao'];
+    $categoria= $_POST['txtCategoria'];
+    $url= $_POST['txtUrl'];
     $botao = $_POST ['botao'];
 
     switch ($botao){
@@ -18,7 +22,35 @@
             echo "<script> location = '../principal.php' </script>";
             break;
         case "editar";
-            
+        echo '<form method="post" action="controller/alterarRestaurante.php" enctype="multipart/form-data">
+        <input type="hidden" name="txtId" value="'.$id.'"/>
+
+        <p>
+            <label>Digite o nome do restaurante: </label>
+            <input type="text" name="txtRestaurante" value="'.$nome.'"/>
+        </p>
+        
+        <p>
+            <label>Descrição: </label>
+            <input type="text" name="txtDescricao" value="'.$descricao.'"/>
+        </p>
+        
+        <p>
+            <label>Categoria: </label>
+            <input type="text" name="txtCategoria" value="'.$categoria.'"/>
+        </p>
+        
+        <p>
+            <label>Imagem: </label>
+            <input type="file" name="arquivo"/>
+        </p>
+        <p>
+            <input type="submit" value="Enviar"/>
+            <input type="reset" value="Limpar"/>
+        </p>
+        
+    
+    </form>';
             break;
         default;
             break;
