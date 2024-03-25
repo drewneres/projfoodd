aeb6cff82a6da0f45ba26c20a4db64bc08e56eef
drewneres <hendrewneresfsa@hotmail.com>
2024-03-25 08:09:10 -0300

    Delete e update

--- dao/manipuladados.php
+++ dao/manipuladados.php
@@ -2,13 +2,41 @@
 
     include("conexao.php");
         class ManipulaDados extends Conexao{
-        private $table;
+        private $table, $fields, $dados, $fieldPK, $valuePK;
         public function getTable(){
             return $this->table;
         }
         public function setTable($t){
             $this->table = $t;
         }
+
+        public function getFields(){
+            return $this->fields;
+        }
+        public function setFields($f){
+            $this->fields = $f;
+        }
+        public function getDados(){
+            return $this->dados;
+        }
+        public function setDados($d){
+            $this->dados = $d;
+        }
+        public function getFieldPK(){
+            return $this->fieldPK;
+        }
+        public function setFieldPK($FP){
+            $this->fieldPK = $FP;
+        }
+        public function getValuePK(){
+            return $this->valuePK;
+        }
+        public function setValuePK($VK){
+            $this->valuePK = $VK;
+        }
+
+
+
         public function getAllDataTable(){
             $this->sql = "select * from $this->table";
             $this->qr = self::execSQL($this->sql);
@@ -25,6 +53,26 @@
             $this->qr = self::execSQL($this->sql);
             $linhas = self::countData($this->qr);
             return $linhas;
+        }
+
+        public function update()
+        {
+            $this->sql = "UPDATE $this->table SET $this->fields WHERE this->fieldPK = '$this->valuePK";
+            if (self::execSQL($this->sql)){
+                $this->status = "Cadastrado com sucesso";
+            } else{
+                $this->status = "Erro ao cadastrar" . mysqli_error($this->connect());
+            }
+        }
+
+        public function delete()
+        {
+            $this->sql = "DELETE FROM $this->table WHERE this->fieldPK = '$this->valuePK";
+            if (self::execSQL($this->sql)){
+                $this->status = "DELETADO com sucesso";
+            } else{
+                $this->status = "Erro ao deletar" . mysqli_error($this->connect());
+            }
         }
     }
 ?>
