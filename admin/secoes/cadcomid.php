b20880b89d88b795e9d8f1969fb78e9989f000c6
drewneres <hendrewneresfsa@hotmail.com>
2024-04-08 10:15:53 -0300

    Criação da tela de cadastrar comida

--- admin/secoes/cadcomid.php
+++ admin/secoes/cadcomid.php
@@ -1,1 +1,24 @@
-<h1> aqui é a comidinha</h1>
+<form method="post" action="controller/inserirComida.php" enctype="multipart/form-data">
+    <p>
+        <label>Nome: </label>
+        <input type="text" name="txtPrato" required>
+    </p>
+    <p>
+        <label>Descrição: </label>
+        <input type="text" name="txtDescricao" required>
+    </p>
+    <p>
+        <label>Preço: </label>
+        <input type="number" name="txtPreco" required>
+    </p>
+    <p>
+        <label>Restaurante: </label>
+        <select name="cbxRest">
+        <option value=""></option>
+        </select>
+    </p>
+    <p>
+        <input type="submit" value="Enviar">
+    </p>
+
+</form>
